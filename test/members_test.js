b966109e0337681227dedb4bca8595698e238ead
Jesse Hixson <hixsonj@gmail.com>
2017-01-29 22:12:39 -0500

    Updates tests to use expect syntax

--- test/members_test.js
+++ test/members_test.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 var chai = require('chai');
-var should = chai.should();
+var expect = chai.expect;
 var chaiHttp = require('chai-http');
 var knex = require('../db/knex');
 var server = require('../index');
@@ -19,10 +19,11 @@ beforeEach(function(done) {
 describe('GET /members', function() {
   it('should return all members', function(done) {
     chai.request(server).get('/members').end(function(err, res) {
-      res.should.have.status(200);
-      res.should.be.json;
-      res.body.data.should.be.a('array');
-      res.body.data.length.should.equal(10);
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body.data).to.be.a('array');
+      expect(res.body.data.length).to.equal(10);
+      expect(res.body.data[0]).to.have.property('subscription');
       done();
     });
   });
